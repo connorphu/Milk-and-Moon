f238d505aebe4227c7acea55b37e95d52192cdee
Connor Phu <connorphu@yahoo.com>
2026-07-06 18:48:17 -0500

    daily summary

---
+++ Web/src/app/features/today/daily-summary/daily-summary.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-daily-summary',
+  imports: [MatCardModule],
+  templateUrl: './daily-summary.html',
+  styleUrl: './daily-summary.css',
+})
+export class DailySummary {}
