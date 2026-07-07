c71450d13fea26fe2bf1f2fed70bdcc51dab85f2
Connor Phu <connorphu@yahoo.com>
2026-07-06 19:19:41 -0500

    Log page layout

--- Web/src/app/features/log/log-page/log-page.ts
+++ Web/src/app/features/log/log-page/log-page.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
-
+import { FeedingForm } from '../feeding-form/feeding-form';
+import { DiaperForm } from '../diaper-form/diaper-form';
+import { SleepForm } from '../sleep-form/sleep-form';
+import { PumpingForm } from '../pumping-form/pumping-form';
 @Component({
   selector: 'app-log-page',
-  imports: [],
+  imports: [FeedingForm, DiaperForm, SleepForm, PumpingForm],
   templateUrl: './log-page.html',
   styleUrl: './log-page.css',
 })
