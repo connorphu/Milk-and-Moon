412cc940dd19835afa302fa4dd8cf351e8099d81
Connor Phu <connorphu@yahoo.com>
2026-07-08 14:10:13 -0500

    Add slider for bottle oz

--- Web/src/app/features/log/feeding-tracker/dialog/feeding-tracker-dialog.ts
+++ Web/src/app/features/log/feeding-tracker/dialog/feeding-tracker-dialog.ts
@@ -1,9 +1,8 @@
 import { Component, inject } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { MatButtonModule } from "@angular/material/button";
 import { MAT_DIALOG_DATA, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef, MatDialogTitle } from "@angular/material/dialog";
 import { MatFormFieldModule } from "@angular/material/form-field";4
-import { MatInputModule } from "@angular/material/input";
+import { MatSliderModule } from "@angular/material/slider";
 
 export interface DialogData {
   name: string;
@@ -14,12 +13,7 @@ export interface DialogData {
   imports: [
     MatFormFieldModule,
     FormsModule,
-    MatButtonModule,
-    MatDialogTitle,
-    MatDialogContent,
-    MatDialogActions,
-    MatDialogClose,
-    MatInputModule
+    MatSliderModule,
   ],
   templateUrl: './feeding-tracker-dialog.html',
   styleUrl: './feeding-tracker-dialog.css',
@@ -27,6 +21,10 @@ export interface DialogData {
 export class FeedingTrackerDialog {
   readonly dialogRef = inject(MatDialogRef<FeedingTrackerDialog>);
   readonly data = inject<DialogData>(MAT_DIALOG_DATA);
+
+  formatLabel(value: number): string {
+    return `${value}oz`;
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
