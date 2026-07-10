c5e1ae12bc1783c5ecf74d0e09608fa7325e40d9
Connor Phu <connorphu@yahoo.com>
2026-07-10 16:59:09 -0500

    Refactor nav buttons

--- Web/src/app/features/log/feeding-tracker/dialog/feeding-tracker-dialog.ts
+++ Web/src/app/features/log/feeding-tracker/dialog/feeding-tracker-dialog.ts
@@ -1,15 +1,16 @@
-import { Component, inject, OnInit, signal, TemplateRef, viewChildren } from "@angular/core";
+import { Component, inject, signal, TemplateRef, viewChild, viewChildren } from "@angular/core";
 import { NgTemplateOutlet } from "@angular/common";
 import { form, FormField } from "@angular/forms/signals";
 import { MatDialogRef } from "@angular/material/dialog";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { MatSliderModule } from "@angular/material/slider";
-import { MatChipListboxChange, MatChipSelectionChange, MatChipsModule } from "@angular/material/chips";
+import { MatChipListboxChange, MatChipsModule } from "@angular/material/chips";
 import { MatTimepickerModule } from "@angular/material/timepicker";
-import { MatStepperModule } from "@angular/material/stepper";
+import { MatStepper, MatStepperModule } from "@angular/material/stepper";
 import { provideNativeDateAdapter } from "@angular/material/core";
 import { TemplateName } from "../../../../shared/directives/template-name";
+import { MatButtonModule } from "@angular/material/button";
 
 export interface FeedingTrackerData {
   bottleSize: number
@@ -40,6 +41,7 @@ interface FeedingStep {
     MatSliderModule,
     MatChipsModule,
     MatTimepickerModule,
+    MatButtonModule
 ],
   templateUrl: './feeding-tracker-dialog.html',
   styleUrl: './feeding-tracker-dialog.css',
@@ -47,6 +49,7 @@ interface FeedingStep {
 export class FeedingTrackerDialog {
   readonly dialogRef = inject(MatDialogRef<FeedingTrackerDialog>);
   readonly allTemplateRefs = viewChildren(TemplateName);
+  readonly feedingStepper = viewChild.required<MatStepper>('stepper')
 
   // step order matters
   readonly breastSteps: FeedingStep[] = [
@@ -74,6 +77,14 @@ export class FeedingTrackerDialog {
 
   protected feedingForm = form(this.feedingModel);
   protected stepType: FeedingStep[] = []
+
+  goBack() {
+    this.feedingStepper().previous()
+  }
+
+  goForward() {
+    this.feedingStepper().next()
+  }
 
   onFeedingTypeChange(event: MatChipListboxChange) {
     this.feedingForm.feedingType().value.set(event.value ?? '')
