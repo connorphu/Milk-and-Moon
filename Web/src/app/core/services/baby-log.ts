9bb4cfc58d1f5f556aee2ab3e72fe8eb23c4c6e8
Connor Phu <connorphu@yahoo.com>
2026-07-23 13:22:18 -0500

    retrieve mock data

--- Web/src/app/core/services/baby-log.ts
+++ Web/src/app/core/services/baby-log.ts
@@ -1,6 +1,39 @@
-import { Service } from '@angular/core';
+import { inject, Service } from '@angular/core';
+import { FeedingTrackerData } from '../models/feeding-log.model';
+import { DiaperTrackerData } from '../models/diaper-log.model';
+import { PumpingTrackerData } from '../models/pumping-log.model';
+import { SleepTrackerData } from '../models/sleep-log.model';
+import { HttpClient } from '@angular/common/http';
+
+interface BabyLogModel {
+  id: string
+  trackerType: 'feeding' | 'diaper' | 'pumping' | 'sleep'
+  createdAt: Date
+  updatedAt: Date
+  data: FeedingTrackerData | DiaperTrackerData | PumpingTrackerData | SleepTrackerData
+}
 
 @Service()
 export class BabyLog {
+  private http = inject(HttpClient)
+
+  loadData() {
+    return this.http.get('/assets/mock-data/baby-log.json')
+  }
+  // trackFeeding(data: FeedingTrackerData): BabyLogModel {
+
+  // }
+
+  // trackDiaper(data: DiaperTrackerData): BabyLogModel {
+
+  // }
+
+  // trackPumping(data: PumpingTrackerData): BabyLogModel {
+
+  // }
+
+  // trackSleep(data: SleepTrackerData): BabyLogModel {
+
+  // }
 
 }
