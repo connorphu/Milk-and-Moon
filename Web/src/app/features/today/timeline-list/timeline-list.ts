797edb245a905b09cbf63ceade5388a1ec55c300
Connor Phu <connorphu@yahoo.com>
2026-07-26 18:04:56 -0500

    load data

--- Web/src/app/features/today/timeline-list/timeline-list.ts
+++ Web/src/app/features/today/timeline-list/timeline-list.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
+import { BabyLog } from '../../../core/services/baby-log';
 
 @Component({
   selector: 'app-timeline-list',
@@ -8,4 +9,15 @@ import { MatListModule } from '@angular/material/list';
   templateUrl: './timeline-list.html',
   styleUrl: './timeline-list.css',
 })
-export class TimelineList {}
+export class TimelineList {
+  private babyLogService = inject(BabyLog)
+
+  public data = {}
+
+  constructor() {
+    this.babyLogService.loadData().subscribe(res => {
+      this.data = res
+      console.log('data: ', this.data)
+    })
+  }
+}
