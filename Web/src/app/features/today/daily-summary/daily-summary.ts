d4e2155a4762454e5a68e6b65a0d3f494cb4ed98
Connor Phu <connorphu@gmail.com>
2026-08-04 12:33:40 -0500

    daily summary

--- Web/src/app/features/today/daily-summary/daily-summary.ts
+++ Web/src/app/features/today/daily-summary/daily-summary.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { DateTime, Duration } from 'luxon';
+import { BabyLogModel } from '../../../core/models/baby-log';
+import { FeedTrackerData } from '../../../core/models/feed-log';
+import { SleepTrackerData } from '../../../core/models/sleep-log';
 
 @Component({
   selector: 'app-daily-summary',
@@ -7,4 +11,45 @@ import { MatCardModule } from '@angular/material/card';
   templateUrl: './daily-summary.html',
   styleUrl: './daily-summary.css',
 })
-export class DailySummary {}
+export class DailySummary {
+  public data = input<BabyLogModel[]>([]);
+
+  readonly feedData = computed(() => {
+    let bottles = 0
+    let breastFeeds = 0
+    const feedData: BabyLogModel[] = this.data().filter(log => log.trackerType === 'feed')
+
+    feedData.forEach(log => {
+      const feedData = log.data as FeedTrackerData
+      if (feedData.feedType === 'bottle') {
+        bottles++
+      } else {
+        breastFeeds++
+      }
+    })
+
+    return { bottles, breastFeeds }
+  })
+
+  readonly diaperData = computed(() => {
+    return this.data().filter(log => log.trackerType === 'diaper')
+  })
+
+  readonly sleepData = computed(() => {
+    const sleepData: BabyLogModel[] = this.data().filter(log => log.trackerType === 'sleep')
+    let totalSeconds = 0
+
+    sleepData.forEach(log => {
+      const sleepData = log.data as SleepTrackerData
+      const startTime = DateTime.fromISO(sleepData.startTime)
+      const endTime = DateTime.fromISO(sleepData.endTime)
+      totalSeconds += endTime.diff(startTime).as('seconds')
+    })
+    const duration = Duration.fromObject({ seconds: totalSeconds }).shiftTo('hours', 'minutes')
+    return { totalHours: duration.hours, totalMinutes: duration.minutes }
+  })
+
+  readonly pumpData = computed(() => {
+    return this.data().filter(log => log.trackerType === 'pump')
+  })
+}
