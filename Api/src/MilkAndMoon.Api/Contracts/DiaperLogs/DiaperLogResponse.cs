8002c77efc99df651b2bedc31bb03994eb585955
Connor Phu <connorphu@gmail.com>
2026-09-17 16:04:59 -0500

    diaperlog endpoints

---
+++ Api/src/MilkAndMoon.Api/Contracts/DiaperLogs/DiaperLogResponse.cs
@@ -0,0 +1,14 @@
+public record DiaperLogResponse(
+    Guid Id,
+    Guid BabyId,
+    DateTimeOffset CreatedAt,
+    DateTimeOffset UpdatedAt,
+    string Timezone,
+    string DiaperType,
+    string? PeeColor,
+    string[] StoolColor,
+    string[] StoolTexture,
+    string? Rash,
+    string[] RashLocation,
+    string Notes
+);
---
+++ Api/src/MilkAndMoon.Api/Contracts/DiaperLogs/UpdateDiaperLogRequest.cs
@@ -0,0 +1,10 @@
+public record UpdateDiaperLogRequest(
+    string? Timezone,
+    string? DiaperType,
+    string? PeeColor,
+    string[] StoolColor,
+    string[] StoolTexture,
+    string? Rash,
+    string[] RashLocation,
+    string? Notes
+);
